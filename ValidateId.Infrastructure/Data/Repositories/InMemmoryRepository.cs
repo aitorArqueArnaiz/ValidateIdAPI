e7a1a25a043b843135d2180f0cb36dd1bd839bb5
aitoredualdnun <aarque@userzoom.com>
2021-10-30 11:54:10 +0200

    Working on in memmory data base design

---
+++ ValidateId.Infrastructure/Data/Repositories/InMemmoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using ValidateId.Infrastructure.Interfaces;
+using NPOI.SS.Formula.Functions;
+
+namespace ValidateId.Infrastructure.Data.Repositories
+{
+    public class InMemmoryRepository : DbContext, IInMemmoryRepository
+    {
+        public DbSet<T> ShoppingBaskets { get; set; }
+
+        public InMemmoryRepository(DbContextOptions options) : base(options)
+        {
+            LoadShoppingBaskets();
+        }
+
+        public void LoadShoppingBaskets()
+        {
+        }
+
+        public List<T> GetShoppingBaskets()
+        {
+            return null;
+        }
+
+        public T AddShoppingBasket()
+        {
+            return null;
+        }
+    }
+}
---
+++ ValidateId.Infrastructure/Interfaces/IInMemmoryRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using NPOI.SS.Formula.Functions;
+using System.Collections.Generic;
+
+namespace ValidateId.Infrastructure.Interfaces
+{
+    public interface IInMemmoryRepository
+    {
+
+        /// <summary>Method that creates the first shopping basket.</summary>
+        void LoadShoppingBaskets();
+
+        /// <summary>Method that gets all the shopping baskets existing in the current in memmory data base.</summary>
+        /// <returns>The list of all shopping baskets/products.</returns>
+        List<T> GetShoppingBaskets();
+    }
+}
