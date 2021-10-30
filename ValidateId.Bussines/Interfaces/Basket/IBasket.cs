5c0010d1c2a8e3e0774681ea4e5750b87329f5fc
aitoredualdnun <aarque@userzoom.com>
2021-10-30 12:37:59 +0200

    Added method summaries to interfaces contracts

--- ValidateId.Bussines/Interfaces/Basket/IBasket.cs
+++ ValidateId.Bussines/Interfaces/Basket/IBasket.cs
@@ -7,8 +7,13 @@ namespace ValidateId.Bussines.Interfaces.Basket
 {
     public interface IBasket
     {
+        /// <summary>Method that gets all the shopping baskets existing in the current in memmory data base.</summary>
+        /// <param name="shoppingBasket">The shopping basket to be added for a given user.</param>
+        /// <returns>The user shopping basket that has been added.</returns>
         ShoppingBasket AddShoppingBasketToUser(ShoppingBasket shoppingBasket);
 
+        /// <summary>Method that gets all the shopping baskets existing in the current inMemmory data base.</summary>
+        /// <returns>The list of all shopping baskets/products.</returns>
         List<ShoppingBasket> GetAllShoppingBaskets();
 
     }
